7f3d220e95f5ff0652a0d4baecf819efe77fb922
nabzstl <nabil@nabilelchami.com>
2025-03-08 12:19:02 -0500

    Fix similarity_df initialization error

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,9 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from recommendation_module import compute_user_similarity
 
 @st.cache_data
 def load_data():
-    user_item_matrix = pd.read_csv('user_item_matrix.csv')
+    user_item_matrix = pd.read_csv("user_item_matrix.csv", index_col=0)
     similarity_df = compute_user_similarity(user_item_matrix)
     return user_item_matrix, similarity_df
--- recommendation_module.py
+++ recommendation_module.py
@@ -1,24 +1,17 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-def compute_user_similarity(user_item_matrix):
-    user_item_pivot = user_item_matrix.pivot_table(index='user_id', columns='item_id', values='rating', fill_value=0)
+def compute_user_similarity(user_item_pivot: pd.DataFrame) -> pd.DataFrame:
     similarity_matrix = cosine_similarity(user_item_pivot)
-    similarity_df = pd.DataFrame(similarity_df, index=user_item_pivot.index, columns=user_item_pivot.index)
+    similarity_df = pd.DataFrame(
+        similarity_matrix, 
+        index=user_item_pivot.index, 
+        columns=user_item_pivot.index
+    )
     return similarity_df
 
 def recommend_items(user_id, user_item_matrix, similarity_df, top_n=3):
-    if user_id not in similarity_df.index:
-        return ["Sorry, user not found."]
-    
-    similar_users = similarity_df[user_id].sort_values(ascending=False).drop(user_id).head(top_n).index
-    user_ratings = user_item_matrix.set_index('user_id')
-    recommended_items = (
-        user_item_matrix[user_item_matrix['user_id'].isin(similarity_df.loc[user_id].nlargest(top_n+1).index)]
-        .groupby('item_id')['rating']
-        .mean()
-        .sort_values(ascending=False)
-        .head(top_n)
-        .index.tolist()
-    )
+    similar_users = similarity_df[user_id].sort_values(ascending=False)[1:]
+    recommendations = user_item_matrix.loc[similar_users.index].mean().sort_values(ascending=False)
+    recommended_items = recommendations.head(top_n).index.tolist()
     return recommended_items
