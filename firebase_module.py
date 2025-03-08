5d01dbd2df3a2cc3bdd4d81c2693001675540c5d
nabzstl <nabil@nabilelchami.com>
2025-03-08 10:49:22 -0500

    Add get_user_feedback and save_user_feedback functions

--- firebase_module.py
+++ firebase_module.py
@@ -1,23 +1,25 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import streamlit as st
 
 def initialize_firebase():
     firebase_creds = {
         "type": st.secrets["firebase"]["type"],
         "project_id": st.secrets["firebase"]["project_id"],
-        "private_key_id": st.secrets["firebase"]["private_key_id"],
         "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
         "client_email": st.secrets["firebase"]["client_email"],
-        "client_id": st.secrets["firebase"]["client_id"],
-        "auth_uri": st.secrets["firebase"].get("auth_uri", "https://accounts.google.com/o/oauth2/auth"),
-        "token_uri": st.secrets["firebase"].get("token_uri", "https://oauth2.googleapis.com/token"),
-        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-        "universe_domain": st.secrets["firebase"].get("universe_domain", "googleapis.com")
+        "token_uri": st.secrets["firebase"]["token_uri"],
     }
-
     cred = credentials.Certificate(firebase_creds)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://nabzai-default-rtdb.firebaseio.com'
-    })
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': st.secrets["firebase"]["database_url"]
+        })
+
+def get_user_feedback(user_id):
+    ref = db.reference(f'feedback/{user_id}')
+    return ref.get()
+
+def save_user_feedback(user_id, feedback):
+    ref = db.reference(f'feedback/{user_id}')
+    ref.set(feedback)
