cec96418772f532b7579173c743abb9b7c5d8206
nabzstl <nabil@nabilelchami.com>
2025-03-08 12:39:49 -0500

    Fix 'db' not defined error by initializing firebase database

--- firebase_module.py
+++ firebase_module.py
@@ -1,18 +1,18 @@
-import streamlit as st
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, db
+import streamlit as st
 
 def initialize_firebase():
     firebase_creds = {
-        "type": st.secrets["firebase"]["type"],
+        "type": "service_account",
         "project_id": st.secrets["firebase"]["project_id"],
         "private_key_id": st.secrets["firebase"]["private_key_id"],
         "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
         "client_email": st.secrets["firebase"]["client_email"],
         "client_id": st.secrets["firebase"]["client_id"],
-        "auth_uri": st.secrets["firebase"]["auth_uri"],
-        "token_uri": st.secrets["firebase"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
     }
 
@@ -23,9 +23,9 @@ def initialize_firebase():
         })
 
 def get_user_feedback(user_id):
-    ref = db.reference(f'/feedback/{user_id}')
+    ref = db.reference(f"user_feedback/{user_id}")
     return ref.get()
 
 def save_user_feedback(user_id, feedback_data):
-    ref = db.reference(f'/feedback/{user_id}')
-    ref.set(feedback_data)
+    ref = db.reference(f"user_feedback/{user_id}")
+    ref.push(feedback_data)
