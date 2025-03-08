5a96ff606db16942d87a457a0ce87566cda58007
nabzstl <nabil@nabilelchami.com>
2025-03-08 10:10:35 -0500

    Add Firebase credentials and initialize Firebase in dashboard

--- firebase_module.py
+++ firebase_module.py
@@ -2,15 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 def initialize_firebase():
-    cred = credentials.Certificate('firebase-adminsdk.json')
+    cred = credentials.Certificate("firebase_credentials.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://nabzai-default-rtdb.firebaseio.com'
     })
-
-def save_user_feedback(user_id, feedback):
-    ref = db.reference(f'/feedback/{user_id}')
-    ref.push(feedback)
-
-def get_user_feedback(user_id):
-    ref = db.reference(f'/feedback/{user_id}')
-    return ref.get()
